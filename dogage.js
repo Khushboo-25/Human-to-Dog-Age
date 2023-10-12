f6aafdaf53dc5b30c469e7d3eef00360a2419732
KHUSHBOO CHAURASIYA <98606333+Khushboo-25@users.noreply.github.com>
2023-10-12 22:23:43 +0530

    Update dogage.js

--- dogage.js
+++ dogage.js
@@ -2,11 +2,24 @@ let humanAge = document.getElementById('humanAge');
 let dogAge = document.getElementById('dogAge');
 
 function solve() {
-    const myAge = humanAge.value;
-    var earlyYears = 2;
+
+    let myAge = humanAge.value;
+
+    // The value saved to this variable will change...
+    let earlyYears = 2;
+    
     earlyYears *= 10.5;
-    myAge -= 2;
-    var laterYears = myAge - 2;
+    
+    let laterYears = myAge - 2;
+    
+    // Number of dog years accounted for by your later years...
     laterYears *= 4;
-    dogAge.textContent = Math.floor((earlyYears + laterYears) * 45 / 9);
+    
+    // console.log(earlyYears)
+    // console.log(laterYears)
+    
+    // This really needs to be explained?
+    let myAgeInDogYears = earlyYears + laterYears;
+    dogAge.textContent = Math.floor(myAgeInDogYears);
 }
+
